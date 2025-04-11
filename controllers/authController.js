12d64efbe400949341c2358b52747c6cacdefac8
JuanJo Andrade <juanjoandpe@hotmail.com>
2025-04-11 09:45:07 -0500

    cambios en authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const config = require('./config/config');
+const config = require('../config/config');
 
 class AuthController {
   static async login(req, res) {
