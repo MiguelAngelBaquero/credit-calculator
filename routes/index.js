dc1c2bc4d95ce4aead846e63ad9c1a8b5642e9fb
JuanJo Andrade <juanjoandpe@hotmail.com>
2025-04-11 10:19:02 -0500

    coment index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const passport = require('passport');
+// const express = require('express');
+// const passport = require('passport');
 
-const userRouter = require('./user.router');
+// const userRouter = require('./user.router');
 
-function routerApi(app) {
-  const router = express.Router();
-  app.use(passport.initialize());
-  app.use('/api/v1', router);
+// function routerApi(app) {
+//   const router = express.Router();
+//   app.use(passport.initialize());
+//   app.use('/api/v1', router);
 
-  router.use('/users', userRouter);
-}
+//   router.use('/users', userRouter);
+// }
 
-module.exports = routerApi;
+// module.exports = routerApi;
